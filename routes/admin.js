f327ae307e867b5546aba8be8babb17bf968ad64
Eric McGrandle <ericmcgrandle@gmail.com>
2020-10-17 17:41:07 +0000

    admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,22 @@
-//TODO
-
 const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get("/pending", (req, res) => {
-    console.log('pending orders');
+
+  //Login
+  router.get("/login", (req, res) => {
+    res.render('login');
   });
+
+  //Pending
+  router.get("/pending", (req, res) => {
+    res.render('pending');
+  });
+
+  //Past_orders
+  router.get("/past_orders", (req, res) => {
+    res.render('past_orders');
+  });
+
   return router;
 };

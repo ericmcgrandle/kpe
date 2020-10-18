f0d929c80db63265ad3b78b80eb9ebee6c5273e5
paigelindahl <paigemarlindahl@gmail.com>
2020-10-18 18:25:45 +0000

    total price updates with deletes

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
   let removeBtn = $('.remove-btn');
-  removeBtn.each(function(index) {
+  removeBtn.each(function() {
     $(this).on('click', function(event) {
       const buttonClicked = event.target;
-      console.log(buttonClicked);
+      const tableRow = buttonClicked.closest('tr');
+      tableRow.remove();
+      updateCartTotal()
+
     })
   })
 });
+
+const updateCartTotal = function() {
+  const cartItems = $('.cart-row');
+  let total = 0;
+  cartItems.each(function() {
+    const cartItem = $(this);
+    const itemPrice = cartItem.find('.item-price');
+    const quantityItem = cartItem.find('.quantity-input');
+    const price = parseInt(itemPrice.text().replace('$', ''));
+    const quantity = quantityItem.val();
+    total = total + (price * quantity);
+  })
+  $('.total-price').html('$' + total + '.00');
+};
 
 
 

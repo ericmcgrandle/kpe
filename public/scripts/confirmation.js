1cc58245a66f0fdb9f9e7b794c73f8e8cc5f2ad7
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-10-22 16:34:45 +0000

    create function to update page when order is ready

--- public/scripts/confirmation.js
+++ public/scripts/confirmation.js
@@ -10,14 +10,35 @@ $(() => {
     renderConfirmation(user);
   });
 
+  $.ajax({
+    method: "GET", 
+    url: `/orderComplete/${orderId}`
+  }).done((data) => {
+    console.log('THIS IS DATA', data)
+    if (data.completed_at) {
+      renderCompleted(data);
+    }
+  })
+
+  const renderCompleted = function(data) {
+    const complete = `
+    <div class="complete-content">
+      <p>Good news... your order is ready for pickup!</p>
+    </div>
+    `
+    // $().remove();
+    $('#confirm-data').append(complete);
+  }
+
   const renderConfirmation = (userData) => {
     const userName = `
       <div class="confirmation-content">
         <h1>Thank You ${userData.name} For Your Order!</h1>
         <p>Stay tuned for the restaurant to confirm your order and return an estimate on the time...</p>
       </div>
-    `
+    `;
     $('#no-user').remove();
     $('#confirm-data').append(userName);
   }
+
 });

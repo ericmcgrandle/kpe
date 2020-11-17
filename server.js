a72001230545a746b9db1183d7b9ed1903fd9e67
Eric McGrandle <ericmcgrandle@gmail.com>
2020-11-17 06:54:39 +0000

    reset

--- server.js
+++ server.js
@@ -10,26 +10,17 @@ const sass       = require("node-sass-middleware");
 const app        = express();
 
 // PG database client/connection setup
-// const { Pool } = require('pg');
-// const dbParams = require('./lib/db.js');
-// const db = new Pool({
-//   user: process.env.DB_USER,
-//   database: process.env.DB_NAME,
-//   host: process.env.DB_HOST,
-//   password: process.env.DB_PASS,
-//   dbParams
-// });
-// db.connect();
-
-const pg = require("pg");
-
-const client = new pg.Client({
-  connectionString: "postgres://pfgahqcqmwxmzv:49d21b31fe161431f8d41717399129ef426c932b225f483f42f13a5435aad174@ec2-52-20-248-222.compute-1.amazonaws.com:5432/der9c83luup649"
+const { Pool } = require('pg');
+const dbParams = require('./lib/db.js');
+const db = new Pool({
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASS,
+  dbParams
 });
+db.connect();
 
-client
-  .connect()
-  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
 
 
 
